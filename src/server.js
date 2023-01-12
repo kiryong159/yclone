9e244dfcc7353e44a411b4bce52c4f6fc318b264
kiryong159 <kiryong159@naver.com>
2023-01-12 14:37:41 +0900

    3-4

--- src/server.js
+++ src/server.js
@@ -2,7 +2,16 @@ import express from "express";
 const PORT = "4000";
 const app = express();
 
-const handleListening = () => console.log("aaa");
+const handleget = (req, res) => {
+  return res.send("<h1>hello this is h1</h1>");
+};
+const handlespen = (req, res) => {
+  return res.send("how cool is that?");
+};
 
+app.get("/", handleget);
+app.get("/span", handlespen);
+
+const handleListening = () => console.log("bbb");
 app.listen(PORT, handleListening);
 // app.listen(4000,  () => console.log("aaa"););
