f1edeb2480f8bce9be6000ab93030dd40cbdbc0d
kiryong159 <kiryong159@naver.com>
2023-01-15 13:48:27 +0900

    5-2

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,7 +1,6 @@
 export const home = (req, res) => res.render("home");
-export const handleVideoEdit = (req, res) =>
-  res.send(`video edit#${req.params.id}`);
-export const seeVideo = (req, res) => res.send(`video see #${req.params.id}`);
+export const handleVideoEdit = (req, res) => res.render("edit");
+export const seeVideo = (req, res) => res.render("watch");
 export const search = (req, res) => res.send("video search");
 export const deleteVideo = (req, res) =>
   res.send(`deleteVideo #${req.params.id}`);
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import Userrouter from "./routers/userRouter";
 const PORT = "4000";
 const app = express();
 app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(morgan("dev"));
 
 app.use("/", GLrouter);
