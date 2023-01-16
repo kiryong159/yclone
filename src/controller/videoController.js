9b2e2e7ded0baa83937e543299f4dfa28282b1ee
kiryong159 <kiryong159@naver.com>
2023-01-16 18:52:15 +0900

    6.1

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,27 +1,31 @@
+const videos = [
+  {
+    name: "First video",
+    score: "5",
+    comment: "2",
+    postAt: "5",
+    views: "200",
+    id: "0",
+  },
+  {
+    name: "second video",
+    score: "4",
+    comment: "1",
+    postAt: "5",
+    views: "440",
+    id: "1",
+  },
+  {
+    name: "third video",
+    score: "3",
+    comment: "7",
+    postAt: "5",
+    views: "260",
+    id: "2",
+  },
+];
+
 export const home = (req, res) => {
-  const videos = [
-    {
-      name: "First video",
-      score: "5",
-      comment: "2",
-      postAt: "5",
-      views: "200",
-    },
-    {
-      name: "second video",
-      score: "4",
-      comment: "1",
-      postAt: "5",
-      views: "440",
-    },
-    {
-      name: "third video",
-      score: "3",
-      comment: "7",
-      postAt: "5",
-      views: "260",
-    },
-  ];
   return res.render("home", {
     pageTitle: "Home",
     potato: "tomato",
@@ -30,8 +34,13 @@ export const home = (req, res) => {
 };
 export const VideoEdit = (req, res) =>
   res.render("edit", { pageTitle: "Video-Edit" });
-export const seeVideo = (req, res) =>
-  res.render("watch", { pageTitle: "Watch" });
+export const seeVideo = (req, res) => {
+  const id = req.params.id;
+  // ES6문법으로 작성시 const { id } = req.params; 이렇게도 가능
+  const nowvideo = videos[id];
+  return res.render("watch", { pageTitle: `Watching ${nowvideo.name}` });
+};
+
 export const search = (req, res) => res.send("video search");
 export const deleteVideo = (req, res) =>
   res.send(`deleteVideo #${req.params.id}`);
