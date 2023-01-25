504c5104e5fda2d58eb5311a190c34726cd10b4c
kiryong159 <kiryong159@naver.com>
2023-01-25 15:02:03 +0900

    7-5

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -16,26 +16,43 @@ export const postJoin = async (req, res) => {
     const ERRMSG = "중복된 유저이름 또는 이메일 입니다.";
     return res.status(400).render("join", { pageTitle: "JJoin", ERRMSG });
   }
-  await User.create({
-    email,
-    username,
-    password,
-    name,
-    location,
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      email,
+      username,
+      password,
+      name,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    ERRMSG = error._message;
+    return res.status(400).render("join", { pageTitle: "JJoin", ERRMSG });
+  }
+};
+
+export const getlogin = (req, res) => {
+  return res.render("login", { pageTitle: "Login" });
+};
+
+export const postlogin = async (req, res) => {
+  const { username, password } = req.body;
+  const exists = await User.exists({ username });
+  const ERRMSG = "존재하지 않는 계정입니다.";
+  if (!exists) {
+    return res.status(400).render("login", { pageTitle: "Login", ERRMSG });
+  }
+  return res.end();
 };
 
 export const Useredit = (req, res) => res.send("User edit");
 
 export const Userremove = (req, res) => res.send("User remove");
 
-export const login = (req, res) => res.send("login");
-
 export const seeUser = (req, res) => res.send("seeUser");
 
 export const logout = (req, res) => res.send("logout");
 
 // res.status( ) 200->OK
-//- 400(Bad Request): 서버가 요청의 구문을 인식하지 못할 때 발생한다
-//- 404(Not Found): 서버가 요청한 페이지를 찾을 수 없을 때 발생한다.
+// - 400(Bad Request): 서버가 요청의 구문을 인식하지 못할 때 발생한다
+// - 404(Not Found): 서버가 요청한 페이지를 찾을 수 없을 때  발생한다
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { home, search } from "../controller/videoController";
-import { getJoin, postJoin, login } from "../controller/userController";
+import {
+  getJoin,
+  postJoin,
+  getlogin,
+  postlogin,
+} from "../controller/userController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getlogin).post(postlogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
