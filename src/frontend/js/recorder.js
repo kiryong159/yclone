ea7b1841ac77606e0fddf90073178f5894454767
kiryong159 <kiryong159@naver.com>
2023-02-09 12:26:16 +0900

    13-3

--- src/frontend/js/recorder.js
+++ src/frontend/js/recorder.js
@@ -1,10 +1,42 @@
 const recordStartbtn = document.getElementById("recordStartbtn");
+const preview = document.getElementById("preview");
+const previewBtn = document.getElementById("previewBtn");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+let recorder;
+
+const handleStart = () => {
+  recordStartbtn.innerText = "Stop Record";
+  recordStartbtn.removeEventListener("click", handleStart);
+  recordStartbtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    const videoFile = URL.createObjectURL(e.data);
+    preview.srcObject = null;
+    preview.src = videoFile;
+    preview.loop = true;
+    preview.play();
+  };
+  recorder.start();
+};
+
+const handleStop = () => {
+  recordStartbtn.innerText = "Download Record";
+  recorder.stop();
+  recordStartbtn.removeEventListener("click", handleStop);
+  recordStartbtn.addEventListener("click", handleDownload);
+};
+
+const handleDownload = () => {};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
+  preview.srcObject = stream;
+  preview.play();
 };
 
+previewBtn.addEventListener("click", init);
 recordStartbtn.addEventListener("click", handleStart);
