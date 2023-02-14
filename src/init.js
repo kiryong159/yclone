7286fcbac9740ad365b2a531c759206e769c08f9
kiryong159 <kiryong159@naver.com>
2023-02-14 20:25:43 +0900

    17.5

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/Video";
 import "./models/User";
 import app from "./server";
 
-const PORT = "4000";
+const PORT = process.env.PORT || "4000";
 const handleListening = () =>
   console.log(`이것은 노드.js 주소는 http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
